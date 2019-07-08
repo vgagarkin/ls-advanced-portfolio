19329f78d657d4456e1fd885523ccd86a8bca9c9
Vladimir Gagarkin <vgagarkin@gmail.com>
2019-07-09 01:35:09 +0300

    – Реализован Parallax с воздушным шаром; – Реализован Parallax с Буддой; – Реализована резиновая верстка до брейкпоинта 1200px; – Исправлены недочеты с 1 недели;

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,10 +1,13 @@
-const parallax = document.querySelector('.welcome-section__parallax-wrapper');
-    layers = parallax.children;
+const parallaxBuddaSection = document.querySelector('.section-budda__parallax-wrapper'),
+    parallaxWelcomeSection = document.querySelector('.welcome-section__parallax-wrapper'),
+    buddaSectionOffsetTop = document.querySelector('.section-budda').offsetTop,
+    layersBuddaSection = parallaxBuddaSection.children,
+    layersWelcomeSection =  parallaxWelcomeSection.children;
 
-function moveLayer(wScroll) {
+function moveLayer(wScroll, layers, multiplier) {
     Array.from(layers).forEach(layer => {
         const speed = layer.dataset.speed,
-            pxl = speed * wScroll / 10;
+            pxl = speed * wScroll / multiplier;
 
         layer.style.transform = 'translate3d(0,-' + pxl + 'px,0)';
     });
@@ -12,6 +15,12 @@ function moveLayer(wScroll) {
 
 
 window.addEventListener('scroll', () => {
-    let wScroll = window.pageYOffset;
-    moveLayer(wScroll);
+    let wScroll = window.pageYOffset,
+        scrollSectionBuddaStartPos = wScroll-buddaSectionOffsetTop;
+
+    if(wScroll > buddaSectionOffsetTop-50) {
+        moveLayer(scrollSectionBuddaStartPos, layersBuddaSection, 50);
+    }
+
+    moveLayer(wScroll, layersWelcomeSection, 40);
 });
