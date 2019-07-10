8b8e70b5d44e1eb7b8c10aafb735781bd8dae48a
Vladimir Gagarkin <vgagarkin@gmail.com>
2019-07-11 01:47:16 +0300

    Реализация блока “Скиллы” на vue.js.

--- src/main.js
+++ src/main.js
@@ -5,3 +5,19 @@ if (process.env.NODE_ENV === "development") {
 
 import "./scripts/skills";
 import "./scripts/parallax";
+
+let navLinks = document.querySelectorAll('.main-menu .main-menu__link');
+
+Array.from(navLinks).forEach(link => {
+  link.onclick = function (e) {
+    e.preventDefault();
+    let sectionToScroll = link.attributes.href.value.substr(1),
+        offsetTopSectionToScroll = document.querySelector('.' + sectionToScroll).offsetTop-50;
+
+    scrollToSection(offsetTopSectionToScroll);
+  }
+});
+
+function scrollToSection(coord) {
+  window.scrollTo(0, coord);
+}
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,50 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+    template: "#skill",
+    props: {
+        skillName: String,
+        skillValue: Number
+    },
+    methods: {
+        fillSkill() {
+            const skillCircle = this.$refs["skill-value"];
+            const circleDashArray = parseInt(
+                getComputedStyle(skillCircle).getPropertyValue("stroke-dasharray")
+            );
+            const circleDashOffset = (circleDashArray / 100) * (100 - this.skillValue);
+
+            skillCircle.style.strokeDashoffset = circleDashOffset;
+        }
+    },
+    mounted() {
+        this.fillSkill();
+    }
+};
+
+const row = {
+    template: "#skills-row",
+    props: {
+        skill: Object
+    },
+    components: {
+        skill
+    }
+};
+
+new Vue({
+    el: "#skills-container",
+    template: "#skills-list",
+    components: {
+        row
+    },
+    data() {
+        return {
+            skills: []
+        }
+    },
+    created() {
+        const data = require("../data/skills.json");
+        this.skills = data;
+    }
+});
