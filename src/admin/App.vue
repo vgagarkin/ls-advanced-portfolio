887cf6d1393d410a4ddab6c3f6b6ccccb82dd19d
Vladimir Gagarkin <vgagarkin@gmail.com>
2019-08-04 19:19:10 +0300

    Реализованы tooltip'ы

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -3,15 +3,36 @@
         app-header
         app-menu
         router-view
+        alert(:class="{'visible' : visible}")
 </template>
 
 
 <script>
+    import {mapState, mapActions, mapGetters} from 'vuex';
+
     export default {
         name: "app",
         components: {
             appHeader: () => import('./components/app-header'),
-            appMenu: () => import('./components/app-menu')
+            appMenu: () => import('./components/app-menu'),
+            alert:() => import('./components/tooltips')
+        },
+        computed:{
+            ...mapState('tooltips', { visible: state => state.visible })
+        },
+        methods:{
+            ...mapActions('tooltips', ['closeTooltip'])
+        },
+        watch:{
+            visible(value){
+                if (value === true){
+                    let timeout;
+                    clearTimeout(timeout);
+                    timeout = setTimeout(() => {
+                        this.closeTooltip();
+                    }, 3000);
+                }
+            }
         }
     }
 </script>
@@ -22,7 +43,7 @@
     @import url("../styles/layout/base.pcss");
 
     .wrapper {
-        max-width: $bp-desktop;
+        max-width: calc($bp-desktop - 120px);
         
         &.ov {
             overflow: visible;
@@ -32,5 +53,96 @@
     .app {
         background-color: $admin-page-bg;
         min-height: 100vh;
+    }
+
+    .admin {
+        &__btn {
+
+            display: block;
+            width: 14px;
+            height: 12px;
+
+            &.edit {
+                background: svg-load("pencil.svg",fill=rgba(#414c63, 1),width=14px,height=12px) center no-repeat;
+            }
+
+            &.save {
+                background: svg-load("tick.svg",fill=rgba(#00d70a, 1),width=14px,height=12px) center no-repeat;
+            }
+
+            &.decline {
+                background: svg-load("remove.svg",fill=rgba(#bf2929, 1),width=14px,height=12px) center no-repeat;
+            }
+
+            &.remove {
+                background: svg-load("trash.svg",fill=rgba(#414c63, 1),width=14px,height=12px) center no-repeat;
+            }
+
+            &.add {
+                display: grid;
+                grid-auto-flow: column;
+                column-gap: 20px;
+                align-items: center;
+                height: 100%;
+                opacity: .7;
+                transition: $transition;
+
+
+                &::before {
+                    content: '+';
+                    background: $admin-page-btn-gradient;
+                    border-radius: 50%;
+                    color: $white;
+                    font-size: 30px;
+                    width: 40px;
+                    height: 40px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    line-height: 1;
+                    font-weight: 600;
+                }
+
+                &:after {
+                    content: attr(data-text);
+                    width: max-content;
+                    color: $admin-active-color;
+                    font-weight: 600;
+                }
+
+                &.small {
+                    &::before {
+                        width: 21px;
+                        height: 21px;
+                        font-size: 14px;
+                    }
+                }
+
+                &:hover {
+                    opacity: 1;
+                }
+            }
+
+            &.disabled {
+                opacity: .5;
+                cursor: no-drop;
+            }
+        }
+
+        &-page {
+            &__title-container {
+                margin: 60px 0;
+                display: grid;
+                grid-template-columns: repeat(2, max-content);
+                column-gap: 60px;
+            }
+
+            &__title {
+                font-size: $font-size-admin-title;
+                font-weight: bold;
+                line-height: 1.62;
+                color: $text-color;
+            }
+        }
     }
 </style>
---
+++ src/admin/components/tooltips.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+    .alert-tooltip__container
+        .alert-tooltip__inner(:class='type')
+            .alert-tooltip__text {{text}}
+</template>
+
+<script>
+    import { mapActions, mapState} from 'vuex';
+    export default {
+        computed:{
+            ...mapState('tooltips',{
+                text : state => state.text,
+                type: state => state.type
+            })
+        },
+        methods:{
+            ...mapActions('tooltips', ['closeTooltip'])
+        }
+    }
+</script>
+
+<style lang="postcss" scoped>
+    .alert-tooltip {
+        &__container {
+            position: fixed;
+            bottom:0;
+            left:0;
+            width:100%;
+            display:flex;
+            justify-content: center;
+            transform:translateY(100%);
+            transition: all 0.2s ease-in-out;
+
+            &.visible{
+                transform:translateY(0);
+            }
+        }
+
+        &__inner {
+            width: 400px;
+            padding: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            color: #fff;
+            font-size: 18px;
+            font-weight: 600;
+            &.success {
+                background-color: #4bb133;
+            }
+            &.overload {
+                background-color: #2d3c4e;
+            }
+            &.error {
+                background-color: #b13333;
+            }
+        }
+    }
+</style>
